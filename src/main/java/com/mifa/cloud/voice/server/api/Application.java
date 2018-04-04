b32dffc8f963a6c0946d90cc1355e5f7ca4845fa
xuanmings <18720987043@139.com>
2018-04-04 16:53:26 +0800

    初始化语音云微服务

---
+++ src/main/java/com/mifa/cloud/voice/server/api/Application.java
@@ -0,0 +1,24 @@
+package com.mifa.cloud.voice.server.api;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableFeignClients
+@EnableAsync			//允许异步任务调度
+@EnableScheduling
+@EnableDiscoveryClient	/*服务注册与发现*/
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
