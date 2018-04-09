41cee94d53c3aa2e30e5542ee7885b51999198c3
xuanmings <18720987043@139.com>
2018-04-09 09:37:42 +0800

    通用返回报文结构体

---
+++ src/main/java/com/mifa/cloud/voice/server/commons/dto/JSONData.java
@@ -0,0 +1,37 @@
+package com.mifa.cloud.voice.server.commons.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * Controller通用返回Json
+ * 
+ * @author 宋烜明
+ *
+ */
+@Data
+@ApiModel("通用响应实体信息")
+public class JSONData {
+	public static final String TRUE 	= "true";
+	public static final String FALSE	= "false";
+
+    @ApiModelProperty("成功标志")
+	private String success;			//正常请求成功标志
+    @ApiModelProperty("错误码")
+	private String errCode;			//错误码
+    @ApiModelProperty("错误信息")
+	private String errMsg;			//错误信息
+	private Object body;	//Body
+
+	public JSONData() {
+		setSuccess(TRUE);
+	}
+
+	public JSONData(String success, String errCode, String errMsg, Object body){
+		this.success = success;
+		this.errCode = errCode;
+		this.errMsg = errMsg;
+		this.body = body;
+	}
+}
