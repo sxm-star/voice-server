a8305c52ed46144c6205b262d9a6c6d8fc05c465
叶齐 <yeqi@51jnq.com>
2018-04-11 16:17:24 +0800

    增加用户审核接口

---
+++ src/main/java/com/mifa/cloud/voice/server/dto/AuthCheckDTO.java
@@ -0,0 +1,26 @@
+package com.mifa.cloud.voice.server.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ * Created by Administrator on 2018/4/11.
+ */
+@Data
+@ApiModel("认证审核入参实体类")
+public class AuthCheckDTO {
+
+    @ApiModelProperty("客户ID")
+    @NotEmpty(message = "客户ID不能为空")
+    private String contractNo;
+
+    @ApiModelProperty("认证状态；2:认证通过,3:认证不通过")
+    @NotEmpty(message = "认证状态不能为空")
+    private String authStatus;
+
+    @ApiModelProperty("审核不通过原因")
+    private String remark;
+
+}
---
+++ src/main/java/com/mifa/cloud/voice/server/dto/AuthCompanyDTO.java
@@ -0,0 +1,44 @@
+package com.mifa.cloud.voice.server.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ * Created by Administrator on 2018/4/11.
+ */
+@Data
+@ApiModel("企业认证入参实体类")
+public class AuthCompanyDTO {
+
+    @ApiModelProperty("用户ID")
+    @NotEmpty(message = "用户ID不能为空")
+    private String contractNo;
+
+    @ApiModelProperty("企业名称")
+    @NotEmpty(message = "企业名称不能为空")
+    private String companyName;
+
+    @ApiModelProperty("企业所在地址")
+    @NotEmpty(message = "企业所在地址不能为空")
+    private String companyAddress;
+
+    @ApiModelProperty("经营年限")
+    private String businessLife;
+
+    @ApiModelProperty("行业")
+    private String profession;
+
+    @ApiModelProperty("官网地址")
+    private String officialWebsiteUrl;
+
+    @ApiModelProperty("营业执照号")
+    @NotEmpty(message = "营业执照号不能为空")
+    private String businessLicenseNo;
+
+    @ApiModelProperty("营业执照")
+    @NotEmpty(message = "营业执照地址不能为空")
+    private String businessLicenseUrl;
+
+}
