2013459509d4445e7f61410e965307687e70e597
叶齐 <yeqi@51jnq.com>
2018-04-11 11:52:29 +0800

    增加修改用户头像、修改用户手机号接口

--- src/main/java/com/mifa/cloud/voice/server/controller/common/VerficationCodeController.java
+++ src/main/java/com/mifa/cloud/voice/server/controller/common/VerficationCodeController.java
@@ -1,13 +1,17 @@
 package com.mifa.cloud.voice.server.controller.common;
 
 import com.alibaba.fastjson.JSONObject;
+import com.mifa.cloud.voice.server.annotation.Loggable;
 import com.mifa.cloud.voice.server.commons.constants.AppConst;
 import com.mifa.cloud.voice.server.commons.dto.CommonResponse;
 import com.mifa.cloud.voice.server.component.RandomSort;
 import com.mifa.cloud.voice.server.component.redis.KeyValueDao;
 import com.mifa.cloud.voice.server.config.StaticConst;
 import com.mifa.cloud.voice.server.dto.MobileVerficationCodeDTO;
+import com.mifa.cloud.voice.server.dto.UserPwdMobileDTO;
+import com.mifa.cloud.voice.server.service.VerficationService;
 import com.mifa.cloud.voice.server.utils.ImageUtil;
+import com.netflix.discovery.converters.Auto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -21,6 +25,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.util.HashMap;
@@ -35,13 +40,17 @@ public class VerficationCodeController {
 
     @Autowired
     private KeyValueDao keyValueDao;
+
     @Autowired
     private ImageUtil imageUtil;
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    @GetMapping(value = "/get_image_verfication_code")
+    @Autowired
+    private VerficationService verficationService;
+
+    @GetMapping(value = "/auth_code/get_image_verfication_code")
     @ApiOperation(value = "图形验证码", notes = "图形验证码")
     @ApiImplicitParams({@ApiImplicitParam(name = "mobile", value = "手机号码", required = true, paramType = "query")})
     public void imageVerficationCode(HttpServletRequest request, HttpServletResponse response){
@@ -65,7 +74,7 @@ public class VerficationCodeController {
         }
     }
 
-    @PostMapping(value = "/get_mobile_verfication_code")
+    @PostMapping(value = "/auth_code/get_mobile_verfication_code")
     @ApiOperation(value = "短信验证码", notes = "短信验证码")
     public CommonResponse mobileVerficationCode(@RequestBody @Valid MobileVerficationCodeDTO param) {
         String code = String.valueOf(RandomSort.generateRandomNum(6));
@@ -81,7 +90,22 @@ public class VerficationCodeController {
         return CommonResponse.successCommonResponse();
     }
 
+    @PostMapping("/auth_code/verify_mobile_code")
+    @ApiOperation(value = "校验手机验证码")
+    @Loggable(descp = "校验手机验证码")
+    public CommonResponse verifyMobileCode(@RequestBody @Valid UserPwdMobileDTO param) {
 
+        // 校验短信验证码
+        String mobileAuthCode = verficationService.getmobileAuthCodeFromCache(param.getMobile());
+        if(StringUtils.isEmpty(mobileAuthCode)) {
+            return CommonResponse.failCommonResponse("手机验证码已过期，请重新获取");
+        }
+        if(!mobileAuthCode.equals(param.getMobileVerficationCode())) {
+            return CommonResponse.failCommonResponse("验证码错误");
+        }
+        return CommonResponse.successCommonResponse();
+
+    }
 
 
 
--- src/main/java/com/mifa/cloud/voice/server/controller/user/UserInfoController.java
+++ src/main/java/com/mifa/cloud/voice/server/controller/user/UserInfoController.java
@@ -3,21 +3,23 @@ package com.mifa.cloud.voice.server.controller.user;
 import com.mifa.cloud.voice.server.annotation.Loggable;
 import com.mifa.cloud.voice.server.commons.constants.AppConst;
 import com.mifa.cloud.voice.server.commons.dto.CommonResponse;
+import com.mifa.cloud.voice.server.dto.UserEditAvatarDTO;
+import com.mifa.cloud.voice.server.dto.UserEditMobileDTO;
 import com.mifa.cloud.voice.server.dto.UserInfoVO;
 import com.mifa.cloud.voice.server.pojo.CustomerLoginInfo;
 import com.mifa.cloud.voice.server.service.CustomerLoginInfoService;
+import com.mifa.cloud.voice.server.service.VerficationService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 
@@ -34,6 +36,8 @@ public class UserInfoController {
     @Autowired
     private CustomerLoginInfoService infoService;
 
+    @Autowired
+    private VerficationService verficationService;
 
 
     @PostMapping("/user_info/get_user_info/{contractNo}")
@@ -58,11 +62,57 @@ public class UserInfoController {
         return CommonResponse.successCommonResponse(vo);
     }
 
+    @PostMapping("/user_info/edit_avatar")
+    @ApiOperation(value = "修改头像")
+    @ApiImplicitParams({@ApiImplicitParam(paramType = "header", name = HttpHeaders.AUTHORIZATION,
+            required = true, value = "service token", dataType = "string")
+    })
+    @Loggable(descp = "修改头像")
+    public CommonResponse editAvatar(@RequestBody @Valid UserEditAvatarDTO param) {
 
+        CustomerLoginInfo customerInfo = infoService.selectByPrimaryKey(param.getContractNo());
+        if(customerInfo == null) {
+            return CommonResponse.failCommonResponse("用户不存在");
+        }
+        customerInfo.setAvatarUrl(param.getAvatarUrl());
+        int count = infoService.updateByPrimaryKeySelective(customerInfo);
 
+        if (count > 0) {
+            return CommonResponse.successCommonResponse("修改头像成功",null);
+        }
+        return CommonResponse.failCommonResponse("修改头像失败");
 
+    }
 
+    @PostMapping("/user_info/edit_mobile")
+    @ApiOperation(value = "修改手机号码")
+    @ApiImplicitParams({@ApiImplicitParam(paramType = "header", name = HttpHeaders.AUTHORIZATION,
+            required = true, value = "service token", dataType = "string")
+    })
+    @Loggable(descp = "修改手机号码")
+    public CommonResponse editMobileVerify(@RequestBody @Valid UserEditMobileDTO param){
 
+        // 校验短信验证码
+        String mobileAuthCode = verficationService.getmobileAuthCodeFromCache(param.getNewMobile());
+        if(StringUtils.isEmpty(mobileAuthCode)) {
+            return CommonResponse.failCommonResponse("手机验证码已过期，请重新获取");
+        }
+        if(!mobileAuthCode.equals(param.getMobieAuthCode())) {
+            return CommonResponse.failCommonResponse("验证码错误");
+        }
+        CustomerLoginInfo customerInfo = infoService.selectByPrimaryKey(param.getContractNo());
+        if(customerInfo == null) {
+            return CommonResponse.failCommonResponse("用户不存在");
+        }
+        customerInfo.setMobile(param.getNewMobile());
+        int count = infoService.updateByPrimaryKeySelective(customerInfo);
+
+        if (count > 0) {
+            return CommonResponse.successCommonResponse("修改手机号成功",null);
+        }
+        return CommonResponse.failCommonResponse("修改手机号失败");
+
+    }
 
 
 
