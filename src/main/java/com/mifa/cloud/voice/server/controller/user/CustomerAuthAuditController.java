4a8ddb5f2335acb061e3ce2b707b87c135fe6f6c
叶齐 <yeqi@51jnq.com>
2018-04-13 16:55:44 +0800

    获得认证审核列表

--- src/main/java/com/mifa/cloud/voice/server/controller/user/CustomerAuthAuditController.java
+++ src/main/java/com/mifa/cloud/voice/server/controller/user/CustomerAuthAuditController.java
@@ -1,7 +1,55 @@
 package com.mifa.cloud.voice.server.controller.user;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageInfo;
+import com.mifa.cloud.voice.server.annotation.Loggable;
+import com.mifa.cloud.voice.server.commons.constants.AppConst;
+import com.mifa.cloud.voice.server.commons.dto.CommonResponse;
+import com.mifa.cloud.voice.server.commons.dto.PageDto;
+import com.mifa.cloud.voice.server.dto.AuthCheckListDTO;
+import com.mifa.cloud.voice.server.dto.CustomerAuthAuditVO;
+import com.mifa.cloud.voice.server.pojo.CustomerAuthAudit;
+import com.mifa.cloud.voice.server.service.CustomerAuthAuditService;
+import com.mifa.cloud.voice.server.utils.BaseBeanUtils;
+import com.netflix.discovery.converters.Auto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Administrator on 2018/4/13.
  */
+@RestController
+@Api(value = "认证审核相关", description = "认证审核相关", produces = MediaType.APPLICATION_JSON)
+@RequestMapping(AppConst.BASE_AUTH_PATH + "v1")
 public class CustomerAuthAuditController {
+
+    @Autowired
+    private CustomerAuthAuditService customerAuthAuditService;
+
+    @PostMapping("/authAudit-list")
+    @ApiOperation(value = "获得认证审核列表")
+    @ApiImplicitParams({@ApiImplicitParam(paramType = "header", name = HttpHeaders.AUTHORIZATION,
+            required = true, value = "service token", dataType = "string")
+    })
+    @Loggable(descp = "获得认证审核列表")
+    public CommonResponse<PageDto<CustomerAuthAuditVO>> getAuthCheckList(@RequestBody(required = false) AuthCheckListDTO param,
+                                                                         @RequestParam(required = true, defaultValue = "1") Integer pageNum,
+                                                                         @RequestParam(required = true, defaultValue = "10") Integer pageSize) {
+        PageDto<CustomerAuthAuditVO> pageDto = customerAuthAuditService.selectAuthCheckList(param, pageNum, pageSize);
+        return CommonResponse.successCommonResponse(pageDto);
+    }
+
+
 }
--- src/main/java/com/mifa/cloud/voice/server/service/CustomerAuthAuditService.java
+++ src/main/java/com/mifa/cloud/voice/server/service/CustomerAuthAuditService.java
@@ -1,14 +1,20 @@
 package com.mifa.cloud.voice.server.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.mifa.cloud.voice.server.commons.dto.PageDto;
 import com.mifa.cloud.voice.server.dao.CustomerAuthAuditMapper;
+import com.mifa.cloud.voice.server.dto.AuthCheckListDTO;
+import com.mifa.cloud.voice.server.dto.CustomerAuthAuditVO;
 import com.mifa.cloud.voice.server.pojo.CustomerAuthAudit;
 import com.mifa.cloud.voice.server.pojo.CustomerAuthAuditExample;
+import com.mifa.cloud.voice.server.utils.BaseBeanUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -54,6 +60,44 @@ public class CustomerAuthAuditService {
         return mapper.updateByPrimaryKeySelective(record);
     }
 
+    /**
+     * 根据条件查询list
+     */
+    public PageDto<CustomerAuthAuditVO> selectAuthCheckList(AuthCheckListDTO dto, Integer page, Integer rows) {
+
+        CustomerAuthAuditExample example = new CustomerAuthAuditExample();
+        CustomerAuthAuditExample.Criteria criteria = example.createCriteria();
+        if(dto != null) {
+            if(StringUtils.isNotEmpty(dto.getAuthType())) {
+                criteria.andAuthTypeEqualTo(dto.getAuthType());
+            }
+            if (StringUtils.isNotEmpty(dto.getCustomerName())) {
+                criteria.andCustomerNameLike("%" + dto.getCustomerName() + "%");
+            }
+            if (StringUtils.isNotEmpty(dto.getMobile())) {
+                criteria.andMobileLike("%" + dto.getMobile() + "%");
+            }
+            if(StringUtils.isNotEmpty(dto.getAuditStatus())) {
+                criteria.andAuditStatusEqualTo(dto.getAuditStatus());
+            }
+        }
+        // 加入分页
+        PageHelper.startPage(page, rows);
+        example.setOrderByClause("auth_time DESC");
+        List<CustomerAuthAudit> list = mapper.selectByExample(example);
+
+        List<CustomerAuthAuditVO> voList = new ArrayList<>();
+        PageInfo<CustomerAuthAudit> pageInfo = new PageInfo<>(list);
+        pageInfo.getList().stream()
+                .forEach(
+                        item -> voList.add(BaseBeanUtils.convert(item, CustomerAuthAuditVO.class))
+                );
+        PageDto<CustomerAuthAuditVO> pageResult =  BaseBeanUtils.convert(pageInfo,PageDto.class);
+        pageResult.setList(voList);
+
+        return pageResult;
+
+    }
 
 
 }
