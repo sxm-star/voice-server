d1ab43ef667947b5406f0726d3ea3c46d507b5c2
xuanmings <18720987043@139.com>
2018-04-20 13:03:14 +0800

    del test dao

--- src/main/java/com/mifa/cloud/voice/server/commons/constants/AppConst.java
+++ src/main/java/com/mifa/cloud/voice/server/commons/constants/AppConst.java
@@ -10,7 +10,7 @@ public interface AppConst {
     String COMMA = ",";
     String SAMPLE_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MjQxMzYzOTcyMzAwODAwIiwibmJmIjoxNDk1MDIxNjQ4LCJpc3MiOiJYSU5CQU5HIiwic2VjVmVyIjoiMSIsInR5cGUiOiJURU5BTlQiLCJleHAiOjE0OTc2MTM2NDgsImlhdCI6MTQ5NTAyMTY0OCwianRpIjoiMGQzZGI5OGY5YmRmNGJkNDkzMTNjYmMzYWM0NTFjN2UifQ.zNryjqJR8q7ynP-lQYIU7WkmvRxFYnJoHm9P6sY4jKc";
     String DOWNLOAD_PATH = "/home/dpwnload/";
-    String DOWNLOAD_FILE_NAME = "test.xls";
+    String DOWNLOAD_FILE_NAME = "xls";
 
     /**
      * 语音模板的meta
