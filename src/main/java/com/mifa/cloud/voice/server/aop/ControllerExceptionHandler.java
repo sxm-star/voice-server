e518737effb7cc869c15936f6b41b6b5b08939ce
xuanmings <18720987043@139.com>
2018-04-23 21:10:23 +0800

    ..

--- src/main/java/com/mifa/cloud/voice/server/aop/ControllerExceptionHandler.java
+++ src/main/java/com/mifa/cloud/voice/server/aop/ControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package com.mifa.cloud.voice.server.aop;
 
 import com.mifa.cloud.voice.server.commons.dto.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,13 +23,13 @@ public class ControllerExceptionHandler {
     public CommonResponse exceptionHandler(Throwable e) {
 
         log.error("异常信息：",e);
-
+        String traceId = MDC.get("CORRELATION_ID");
         if (e instanceof MethodArgumentNotValidException) {
             return CommonResponse.failCommonResponse("400", ((MethodArgumentNotValidException) e).getBindingResult().getFieldError()
-                    .getDefaultMessage());
+                    .getDefaultMessage(),traceId);
         }
 
-        return CommonResponse.failCommonResponse("400", e.getCause() != null ? e.getCause().getMessage() : e.getMessage());
+        return CommonResponse.failCommonResponse("400", e.getCause() != null ? e.getCause().getMessage() : e.getMessage(),traceId);
     }
 
 
