7bb9e692c0f1e8b47c4b2f483ccd893140a90b13
xuanmings <18720987043@139.com>
2018-05-11 18:17:46 +0800

    .

--- src/main/java/com/mifa/cloud/voice/server/pojo/VoiceNotifyLogDO.java
+++ src/main/java/com/mifa/cloud/voice/server/pojo/VoiceNotifyLogDO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -22,7 +23,7 @@ public class VoiceNotifyLogDO extends BaseDataDo {
      * 主键ID
      */
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     /**
