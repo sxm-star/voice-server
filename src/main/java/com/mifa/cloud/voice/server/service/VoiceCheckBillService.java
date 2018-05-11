1555a111283f9136213ff51e7c4dad88b3344589
xuanmings <18720987043@139.com>
2018-05-11 18:12:15 +0800

    .

--- src/main/java/com/mifa/cloud/voice/server/service/VoiceCheckBillService.java
+++ src/main/java/com/mifa/cloud/voice/server/service/VoiceCheckBillService.java
@@ -1,11 +1,13 @@
 package com.mifa.cloud.voice.server.service;
 
 import com.mifa.cloud.voice.server.pojo.VoiceCheckBillLogDO;
+import org.springframework.stereotype.Service;
 
 /**
  * @author: songxm
  * @date: 2018/5/11 17:25
  * @version: v1.0.0
  */
+@Service
 public class VoiceCheckBillService extends BaseService<VoiceCheckBillLogDO>{
 }
