dd6439c63120ceaccf68aed1a802394d5320c03d
xuanmings <18720987043@139.com>
2018-06-01 09:19:03 +0800

    测试

---
+++ src/main/java/com/mifa/cloud/voice/server/commons/dto/CallStatisticsDTO.java
@@ -0,0 +1,9 @@
+package com.mifa.cloud.voice.server.commons.dto;
+
+/**
+ * @author: songxm
+ * @date: 2018/6/1 08:33
+ * @version: v1.0.0
+ */
+public class CallStatisticsDTO {
+}
---
+++ src/main/java/com/mifa/cloud/voice/server/commons/event/CallStatisticsEvent.java
@@ -0,0 +1,9 @@
+package com.mifa.cloud.voice.server.commons.event;
+
+/**
+ * @author: songxm
+ * @date: 2018/6/1 08:35
+ * @version: v1.0.0
+ */
+public class CallStatisticsEvent {
+}
